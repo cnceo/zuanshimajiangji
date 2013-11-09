39d2be7a90af44b3b668a526c258c7255be07947
elephantYunyao <elephant@yunyao-inc.com>
2013-11-09 13:55:24 +0800

    addproject

---
+++ 钻石麻将/AsyncButton.h
@@ -0,0 +1,13 @@
+//
+//  AsyncButton.h
+//  钻石麻将
+//
+//  Created by 向 文品 on 13-11-9.
+//  Copyright (c) 2013年 Junsheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AsyncButton : UIButton
+
+@end
---
+++ 钻石麻将/AsyncButton.m
@@ -0,0 +1,31 @@
+//
+//  AsyncButton.m
+//  钻石麻将
+//
+//  Created by 向 文品 on 13-11-9.
+//  Copyright (c) 2013年 Junsheng. All rights reserved.
+//
+
+#import "AsyncButton.h"
+
+@implementation AsyncButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
